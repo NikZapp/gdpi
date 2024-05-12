8a718962354d04a6bcb10fe21d65257d8738d4fa
NikZapp <58770289+NikZapp@users.noreply.github.com>
2024-05-12 20:42:51 +0500

    Implement basic protocol encoding/decoding

---
+++ pi_protocol.gd
@@ -0,0 +1,162 @@
+extends Object
+static var protocol_path = "res://pi_protocol/pi_protocol/data/protocol.json"
+static var name_to_id : Dictionary
+static var packet_data : Dictionary
+
+static var encoded_packet : PackedByteArray = []
+
+static func load_protocol() -> void:
+	encoded_packet.resize(64)
+	var data = JSON.parse_string(FileAccess.get_file_as_string(protocol_path))
+	for packet_name in data:
+		var packet_info = data[packet_name]
+		var packet_id = int(packet_info["id"])
+		name_to_id[packet_name] = packet_id
+		packet_data[packet_id] = {
+			"packet_id": packet_id,
+			"packet_name": packet_name,
+			"fields": {}
+		}
+		# TODO: Profile if reversing the PackedByteArray is faster than reading
+		# it with a custom function.
+		for field in packet_info["fields"].keys():
+			var field_type = packet_info["fields"][field]
+			packet_data[packet_id]["fields"][field.to_snake_case()] = field_type
+	print(packet_data)
+	
+	
+
+static func decode(data : PackedByteArray):
+	if data.size() == 0:
+		return {
+			"packet_name": "EmptyPacket",
+			"packet_id": -1
+		}
+		
+	var packet_template = packet_data.get(data[0])
+	if (!packet_template):
+		return {
+			"packet_name": "UnknownPacket",
+			"packet_id": data[0],
+			"data": data
+		}
+	
+	var decoded_packet = {
+		"packet_name": packet_template["packet_name"],
+		"packet_id": packet_template["packet_id"]
+	}
+	
+	data.reverse() # Convert to Little-Endian
+	var cursor = data.size() - 2
+	for field_name in packet_template["fields"].keys():
+		var field_type = packet_template["fields"][field_name]
+		match field_type:
+			"Byte":
+				decoded_packet[field_name] = data.decode_s8(cursor)
+				cursor -= 1
+			"UnsignedByte":
+				decoded_packet[field_name] = data.decode_u8(cursor)
+				cursor -= 1
+			"ShortBE":
+				decoded_packet[field_name] = data.decode_s16(cursor - 1)
+				cursor -= 2
+			"UnsignedShortBE":
+				decoded_packet[field_name] = data.decode_u16(cursor - 1)
+				cursor -= 2
+			"IntBE":
+				decoded_packet[field_name] = data.decode_s32(cursor - 3)
+				cursor -= 4
+			"UnsignedIntBE":
+				decoded_packet[field_name] = data.decode_u32(cursor - 3)
+				cursor -= 4
+			"FloatBE":
+				decoded_packet[field_name] = data.decode_float(cursor - 3)
+				cursor -= 4
+			"UnsignedLongBE":
+				decoded_packet[field_name] = data.decode_u64(cursor - 7)
+				cursor -= 7
+			"String":
+				var length = data.decode_u16(cursor - 1)
+				cursor -= 2
+				var s = ""
+				for i in length:
+					s += char(data.decode_u8(cursor))
+					cursor -= 1
+				decoded_packet[field_name] = s
+			_:
+				print("Unknown field type during decoding: ", field_type, " in packet ", packet_template["packet_name"])
+	
+	return decoded_packet
+
+static func encode(packet_id, data : Dictionary) -> PackedByteArray:
+	if typeof(packet_id) == TYPE_STRING:
+		packet_id = name_to_id[packet_id]
+	var packet_template = packet_data.get(packet_id)
+	if (!packet_template):
+		var packet : PackedByteArray = [packet_id]
+		packet.append_array(data.get("data", PackedByteArray()))
+		return packet
+	
+	# Packing encoding must be forward, because resizing it all the time would
+	# be inefficient. Therefore, we only resize at 2^n sizes
+	encoded_packet[0] = packet_id
+	var cursor = 1
+	for field_name in packet_template["fields"].keys():
+		if cursor + 16 > encoded_packet.size():
+			# Make the shared buffer larger when approaching size limit
+			encoded_packet.resize(encoded_packet.size() * 2)
+		var field_type = packet_template["fields"][field_name]
+		var value = data.get(field_name, 0)
+		match field_type:
+			"Byte":
+				encoded_packet.encode_s8(cursor, value)
+				cursor += 1
+			"UnsignedByte":
+				encoded_packet.encode_u8(cursor, value)
+				cursor += 1
+			"ShortBE":
+				encoded_packet.encode_s16(cursor, value)
+				reverse_endianness(encoded_packet, cursor, 2)
+				cursor += 2
+			"UnsignedShortBE":
+				encoded_packet.encode_u16(cursor, value)
+				reverse_endianness(encoded_packet, cursor, 2)
+				cursor += 2
+			"IntBE":
+				encoded_packet.encode_s32(cursor, value)
+				reverse_endianness(encoded_packet, cursor, 4)
+				cursor += 4
+			"UnsignedIntBE":
+				encoded_packet.encode_u32(cursor, value)
+				reverse_endianness(encoded_packet, cursor, 4)
+				cursor += 4
+			"FloatBE":
+				encoded_packet.encode_float(cursor, value)
+				reverse_endianness(encoded_packet, cursor, 4)
+				cursor += 4
+			"UnsignedLongBE":
+				encoded_packet.encode_u64(cursor, value)
+				reverse_endianness(encoded_packet, cursor, 8)
+				cursor += 8
+			"String":
+				var length = len(value)
+				encoded_packet.encode_s16(cursor, length)
+				reverse_endianness(encoded_packet, cursor, 2)
+				cursor += 2
+				var buf = value.to_ascii_buffer()
+				for i in length:
+					encoded_packet[cursor] = buf[i]
+					cursor += 1
+			_:
+				print("Unknown field type during encoding: ", field_type, " in packet ", packet_template["packet_name"])
+	return encoded_packet.slice(0, cursor)
+
+static func to_id(name : String) -> int:
+	return name_to_id.get(name, -1)
+
+static func reverse_endianness(buffer : PackedByteArray, start : int, length : int) -> void:
+	var temp
+	for i in (length >> 1):
+		temp = buffer[i + start]
+		buffer[i + start] = buffer[length-i-1 + start]
+		buffer[length-i-1 + start] = temp
