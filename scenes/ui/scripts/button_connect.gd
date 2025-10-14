392407980ff27cb8863e72be6df21bb249a203f6
NikZapp <dev.nikzapp@gmail.com>
2025-10-15 01:23:11 +0500

    update to 4.5 + pixelated shadows

--- scenes/ui/scripts/button_connect.gd
+++ scenes/ui/scripts/button_connect.gd
@@ -7,9 +7,14 @@ extends Button
 
 func _on_pressed() -> void:
 	Global.username = line_edit_username.text
-	if Global.username == "":
-		Global.username = "GodotPi"
 	Global.ip = line_edit_ip.text
 	Global.port = int(line_edit_port.text)
+	
+	if Global.username == "":
+		Global.username = "GodotPi"
+	if Global.ip == "":
+		Global.ip = "localhost"
+	if Global.port == 0:
+		Global.port = 19132
 	
 	get_tree().change_scene_to_file("res://scenes/game_world.tscn")
